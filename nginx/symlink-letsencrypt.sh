ade7f53f83982c4e03e823caf78e22fa99fc20b2
golopot <golopot@gmail.com>
2018-07-11 11:28:59 +0800

    Add nginx.d files

--- nginx/symlink-letsencrypt.sh
+++
@@ -1,2 +0,0 @@
-ln -sf /etc/letsencrypt/live/krust.hopto.org/fullchain.pem site.crt
-ln -sf /etc/letsencrypt/live/krust.hopto.org/privkey.pem site.key
--- scripts/deploy.sh
+++
@@ -1,1 +0,0 @@
-ssh 35.189.134.4 "bash -c 'cd ~/potato; git pull; sudo docker-compose up --build -d'"
